32a60af0aa57db59cc8829d4c8db35633f767b8b
Linda Nguyen <skoryky@gmail.com>
2016-07-12 18:28:11 -0700

    Add Settings model for storing configuration values

--- codereview/models.py
+++ codereview/models.py
@@ -1588,3 +1588,24 @@ def sum_account_statistics(out, items):
       prev_latencies != out.latencies or
       prev_lgtms != out.lgtms or
       prev_review_types != out.review_types)
+
+class Settings(ndb.Model):
+  name = ndb.StringProperty()
+  value = ndb.StringProperty()
+
+  @staticmethod
+  def get(name):
+    NOT_SET_VALUE = "NOT SET"
+    retval = Settings.query(Settings.name == name).get()
+    if not retval:
+      retval = Settings()
+      retval.name = name
+      retval.value = NOT_SET_VALUE
+      retval.put()
+    if retval.value == NOT_SET_VALUE:
+      raise Exception(('Setting %s not found in the database. A placeholder ' +
+        'record has been created. Go to the Developers Console for your app ' +
+        'in App Engine, look up the Settings record with name=%s and enter ' +
+        'its value in that record\'s value field.') % (name, name))
+    return retval.value
+
